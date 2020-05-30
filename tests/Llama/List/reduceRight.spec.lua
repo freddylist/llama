ac87eea8ec4f7d1d026ed9b43012cd54f3423afd
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 18:03:04 -0500

    Added list.reduceRight test

---
+++ tests/Llama/List/reduceRight.spec.lua
@@ -0,0 +1,67 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local List = Llama.List
+	local reduceRight = List.reduceRight
+
+	it("should call the reducer", function()
+		local a = {1, 2, 3}
+		local called = 0
+
+		reduceRight(a, function()
+			called = called + 1
+		end, 0)
+
+		expect(called).to.equal(3)
+	end)
+
+	it("should not call the reducer when the list is empty", function()
+		local called = false
+
+		reduceRight({}, function()
+			called = true
+		end, 0)
+
+		expect(called).to.equal(false)
+	end)
+
+	it("should call the reducer for each element", function()
+		local a = {4, 5, 6}
+		local copy = {}
+
+		reduceRight(a, function(accum, value, index)
+			copy[index] = value
+			return accum
+		end, 0)
+
+		expect(#copy).to.equal(#a)
+
+		for key, value in pairs(a) do
+			expect(value).to.equal(copy[key])
+		end
+	end)
+
+	it("should pass the same modified initial value to the reducer", function()
+		local a = {5, 4, 3}
+		local initialValue = {}
+
+		reduceRight(a, function(accum)
+			expect(accum).to.equal(initialValue)
+			return accum
+		end, initialValue)
+	end)
+
+	it("should call the reducer in the correct order", function()
+		local a = {5, 4, 3}
+		local index = 3
+
+		reduceRight(a, function(accum, value)
+			expect(value).to.equal(a[index])
+			index = index - 1
+			return accum
+		end, 0)
+	end)
+end
