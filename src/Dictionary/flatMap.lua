e7aba669e2ef752f94211b9e7ee48d486eec2492
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:43:20 -0500

    added Dictionary.flatMap

---
+++ src/Dictionary/flatMap.lua
@@ -0,0 +1,20 @@
+
+local join = require(script.Parent:WaitForChild("join"))
+
+local function flatMap(dictionary, mapper)
+	local new = {}
+
+	for k, v in pairs(dictionary) do
+		if type(v) == "table" then
+			new = join(flatMap(v, mapper), new)
+		else
+			local value, key = mapper(v, k)
+
+			new[key or k] = value
+		end
+	end
+	
+	return new
+end
+
+return flatMap
