caee3e7ee6fd115934bc574552c1773382f43310
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:42:47 -0500

    Added Dictionary.equalsDeep

---
+++ src/Dictionary/equalsDeep.lua
@@ -0,0 +1,32 @@
+
+local equalObjects = require(script.Parent.Parent:WaitForChild("EqualObjects"))
+
+local function equalsDeep(...)
+	if equalObjects(...) then
+		return true
+	end
+
+	local argc = select('#', ...)
+
+	for i = 1, argc do
+		local dictionary = select(i, ...)
+
+		for j = 1, argc do
+			if j ~= i then
+				local compare = select(j, ...)
+				
+				for k, v in pairs(dictionary) do
+					if type(v) == "table" and type(compare[k]) == "table" and not equalsDeep(v, compare[k]) then
+						return false
+					elseif v ~= compare[k] then
+						return false
+					end
+				end
+			end
+		end
+	end
+
+	return true
+end
+
+return equalsDeep
