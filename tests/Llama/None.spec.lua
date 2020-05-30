6dc5de941c585ed95300da9af2e267bf602d4b97
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 18:01:40 -0500

    Added None test

---
+++ tests/Llama/None.spec.lua
@@ -0,0 +1,18 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+	local None = Llama.None
+
+	it("should be a userdata", function()
+		expect(None).to.be.a("userdata")
+	end)
+
+	it("should have a nice string name", function()
+		local coerced = tostring(None)
+
+		expect(coerced:find("^userdata: ")).never.to.be.ok()
+		expect(coerced:find("None")).to.be.ok()
+	end)
+end
