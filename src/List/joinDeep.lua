65fac0351f556180ac39d833145ad2ae985a5fd9
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:49:57 -0500

    Added List.joinDeep

---
+++ src/List/joinDeep.lua
@@ -0,0 +1,29 @@
+
+local None = require(script.Parent.Parent:WaitForChild("None"))
+local copyDeep = require(script.Parent:WaitForChild("copyDeep"))
+
+local function joinDeep(...)
+	local new = {}
+
+	for listIndex = 1, select("#", ...) do
+		local list = select(listIndex, ...)
+
+		for i = 1, #list do
+			if list[i] == None then
+				new[i] = nil
+			elseif type(list[i]) == "table" then
+				if new[i] == nil or type(new[i] ~= "table") then
+					new[i] = copyDeep(list[i])
+				else
+					new[i] = joinDeep(new[i], list[i])
+				end
+			else
+				new[i] = list[i]
+			end
+		end
+	end
+
+	return new
+end
+
+return joinDeep
