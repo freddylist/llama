8b2e3f69b5753d62a3842768b84015f9ef7a0b16
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:45:25 -0500

    Added Dictionary for splitting up methods

---
+++ src/Dictionary/init.lua
@@ -0,0 +1,25 @@
+
+local Dictionary = {
+	copy		= require(script:WaitForChild("copy")),
+	copyDeep	= require(script:WaitForChild("copyDeep")),
+	count		= require(script:WaitForChild("count")),
+	equals		= require(script:WaitForChild("equals")),
+	equalsDeep	= require(script:WaitForChild("equalsDeep")),
+	every		= require(script:WaitForChild("every")),
+	filter		= require(script:WaitForChild("filter")),
+	flatMap		= require(script:WaitForChild("flatMap")),
+	flatten		= require(script:WaitForChild("flatten")),
+	flip		= require(script:WaitForChild("flip")),
+	includes	= require(script:WaitForChild("includes")),
+	join		= require(script:WaitForChild("join")),
+	joinDeep	= require(script:WaitForChild("joinDeep")),
+	keys		= require(script:WaitForChild("keys")),
+	map			= require(script:WaitForChild("map")),
+	remove 		= require(script:WaitForChild("remove")),
+	set			= require(script:WaitForChild("set")),
+	some		= require(script:WaitForChild("some")),
+	update		= require(script:WaitForChild("update")),
+	values		= require(script:WaitForChild("values")),
+}
+
+return Dictionary
