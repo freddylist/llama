cfcb9233a8ca488c83842652b4dde3a0b43608eb
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:49:51 -0500

    Added List.join

---
+++ src/List/join.lua
@@ -0,0 +1,22 @@
+
+local None = require(script.Parent.Parent:WaitForChild("None"))
+
+local function join(...)
+	local new = {}
+
+	for listIndex = 1, select("#", ...) do
+		local list = select(listIndex, ...)
+
+		for i = 1, #list do
+			if list[i] == None then
+				new[i] = nil
+			else
+				new[i] = list[i]
+			end
+		end
+	end
+
+	return new
+end
+
+return join
