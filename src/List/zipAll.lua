423709e3a9d6613e91b94db7995eb7862a849f0a
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 10:51:58 -0500

    Added List.zipAll

---
+++ src/List/zipAll.lua
@@ -0,0 +1,32 @@
+
+local None = require(script.Parent.Parent:WaitForChild("None"))
+
+local function zipAll(...)
+	local new = {}
+	local argc = select('#', ...)
+	local maxLen = #select(1, ...)
+
+	for i = 2, argc do
+		local len = #select(i, ...)
+
+		if len > maxLen then
+			maxLen = len
+		end
+	end
+
+	for i = 1, maxLen do
+		for j = 1, argc do
+			local value = select(j, ...)[i]
+
+			if value == nil then
+				new[i][j] = None
+			else
+				new[i][j] = select(j, ...)[i]
+			end
+		end
+	end
+
+	return new
+end
+
+return zipAll
