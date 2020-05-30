57919542aeecda53ac13fde398c123ed6be9f1ca
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 18:59:56 -0500

    Added List.delete test

---
+++ tests/Llama/List/delete.spec.lua
@@ -0,0 +1,60 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local None = Llama.None
+
+	local List = Llama.List
+	local delete = List.delete
+
+	it("should remove the element at the given index", function()
+		local a = {
+			"first",
+			"second",
+			"third"
+		}
+
+		local b = delete(a, 2)
+
+		expect(#b).to.equal(2)
+		expect(b[1]).to.equal("first")
+		expect(b[2]).to.equal("third")
+	end)
+
+	it("should not remove any element if index is out of bound", function()
+		local a = {
+			"first",
+			"second",
+			"third"
+		}
+		local b = delete(a, 4)
+
+		expect(#b).to.equal(#a)
+		for i = 1, #a do
+			expect(b[i]).to.equal(a[i])
+		end
+
+		local c = delete(a, -2)
+
+		expect(#c).to.equal(#a)
+		for i = 1, #a do
+			expect(c[i]).to.equal(a[i])
+		end
+	end)
+
+	it("should work with a None element", function()
+		local a = {
+			"first",
+			None,
+			"third"
+		}
+
+		local b = delete(a, 1)
+
+		expect(#b).to.equal(2)
+		expect(b[1]).to.equal(None)
+		expect(b[2]).to.equal("third")
+	end)
+end
