677a118878475e75b83f333bfdf6060762bf8cd0
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-05-30 19:00:03 -0500

    Added List.set test

--- src/List/set.lua
+++ src/List/set.lua
@@ -1,9 +1,14 @@
 
 local function set(list, index, value)
+	local len = #list
+
+	assert(index <= len, "index must be less than or equal to the list length")
+	assert(index >= 1, "index must be greater than or equal to 1")
+
 	local new = {}
 	local resultIndex = 1
 
-	for i = 1, #list do
+	for i = 1, len do
 		if i == index then
 			new[resultIndex] = value
 		else
@@ -12,6 +17,8 @@ local function set(list, index, value)
 
 		resultIndex = resultIndex + 1
 	end
+
+	return new
 end
 
 return set
