742cf998f2d7fb7188a080551804b6b1febce27b
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-01 16:16:21 -0500

    Added ability to remove more than one index

--- src/List/delete.lua
+++ src/List/delete.lua
@@ -1,11 +1,16 @@
 
-local copy = require(script.Parent.copy)
+local toSet = require(script.Parent.toSet)
 
 local function delete(list, ...)
-	local new = copy(list)
+	local new = {}
+	local removeIndices = toSet({...})
+	local index = 1
 
-	for i = 1, select('#', ...) do
-		new[select(i, ...)] = nil
+	for i = 1, #list do
+		if not removeIndices[i] then
+			new[index] = list[i]
+			index = index + 1
+		end
 	end
 
 	return new
