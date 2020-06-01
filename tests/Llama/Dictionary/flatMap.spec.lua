402c7411bad239c1823cf8fd21efcce77e73e817
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-01 16:18:26 -0500

    Added Dictionary.flatMap test

---
+++ tests/Llama/Dictionary/flatMap.spec.lua
@@ -0,0 +1,10 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local Dictionary = Llama.Dictionary
+	local filter = Dictionary.filter
+
+end
