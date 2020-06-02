a364156d6fddd53ef90d753c79bc912714af8336
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 12:07:35 -0500

    Fixed and optimized List.flatten

--- src/List/flatten.lua
+++ src/List/flatten.lua
@@ -1,17 +1,22 @@
 
-local join = require(script.Parent.join)
-
 local function flatten(list)
 	local listType = type(list)
 	assert(listType == "table", "expected a table for first argument, got " .. listType)
 	
 	local new = {}
+	local index = 1
 
 	for i = 1, #list do
 		if type(list[i]) == "table" then
-			new = join(flatten(list[i]), new)
+			local layer = flatten(list[i])
+
+			for j = 1, #layer do
+				new[index] = layer[j]
+				index = index + 1
+			end
 		else
-			new[i] = list[i]
+			new[index] = list[i]
+			index = index + 1
 		end
 	end
 
