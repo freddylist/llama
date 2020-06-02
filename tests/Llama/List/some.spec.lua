f5dce730ad762c65caec53a46df7447047fcea4c
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 12:11:32 -0500

    Added List.some test

---
+++ tests/Llama/List/some.spec.lua
@@ -0,0 +1,62 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local List = Llama.List
+	local some = List.some
+
+	it("should return a boolean", function()
+		local function always(bool)
+			return function()
+				return bool
+			end
+		end
+
+		expect(some({}, always(true))).to.be.a("boolean")
+		expect(some({}, always(false))).to.be.a("boolean")
+	end)
+
+	it("should pass the value and index to the predicate", function()
+		local a = {
+			"bar",
+		}
+
+		local function predicate(v, i)
+			expect(v).to.equal(a[1])
+			expect(i).to.equal(1)
+		end
+
+		some(a, predicate)
+	end)
+
+	it("should return true if at least one entry passes the predicate", function()
+		local a = {
+			"a",
+			"b",
+			"c",
+		}
+
+		local function aOnly(v)
+			return v == "a"
+		end
+
+		expect(some(a, aOnly)).to.equal(true)
+	end)
+
+	it("should return false if no entries pass the predicate", function()
+		local a = {
+			"a",
+			"a",
+			"a",
+			"b",
+		}
+
+		local function cOnly(v)
+			return v == "c"
+		end
+
+		expect(some(a, cOnly)).to.equal(false)
+	end)
+end
