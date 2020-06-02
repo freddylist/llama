5886264f01464cf4feddebb6ef688a62ed16f6d2
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 12:09:43 -0500

    Added Dictionary.set test

---
+++ tests/Llama/Dictionary/set.spec.lua
@@ -0,0 +1,45 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local Dictionary = Llama.Dictionary
+	local set = Dictionary.set
+
+	it("should return a new table", function()
+		local a = {}
+
+		expect(set(a, "")).never.to.equal(a)
+	end)
+
+	it("should not mutate the given table", function()
+		local a = {
+			foo = "foo",
+			bar = "bar",
+			baz = "baz"
+		}
+		local mutations = 0
+
+		setmetatable(a, {
+			__newindex = function()
+				mutations = mutations + 1
+			end,
+		})
+
+		set(a, "baz", "boot")
+
+		expect(mutations).to.equal(0)
+	end)
+
+	it("should set value at specified key", function()
+		local a = {
+			foo = "foo",
+			bar = "bar",
+		}
+
+		local b = set(a, "bar", "boot")
+
+		expect(b.bar).to.equal("boot")
+	end)
+end
