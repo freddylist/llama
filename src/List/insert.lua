261841e4d8101c70384002454a005fe1c34b6018
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 13:07:11 -0500

    Added assertion for out of bounds index

--- src/List/insert.lua
+++ src/List/insert.lua
@@ -3,10 +3,13 @@ local function insert(list, index, ...)
 	local listType = type(list)
 	assert(listType == "table", "expected a table for first argument, got " .. listType)
 
+	local len = #list
+	assert(index >= 1 and index <= len, "index must be in bounds of list")
+
 	local new = {}
 	local resultIndex = 1
 	
-	for i = 1, #list do
+	for i = 1, len do
 		if i == index then
 			for j = 1, select('#', ...) do
 				new[resultIndex] = select(j, ...)
