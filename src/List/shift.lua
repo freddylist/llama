9952baada760fcffddeff20693805dadda4430c7
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 13:06:49 -0500

    Added ability to pop and shift multiple values

--- src/List/shift.lua
+++ src/List/shift.lua
@@ -1,12 +1,16 @@
 
-local function shift(list)
+local function shift(list, shifts)
 	local listType = type(list)
 	assert(listType == "table", "expected a table for first argument, got " .. listType)
+
+	shifts = shifts or 1
+	local shiftsType = type(shifts)
+	assert(shiftsType == "number" and shiftsType % 1 == 0, "expected an integer for second argument, got " .. shiftsType)
 	
 	local new = {}
 
-	for i = 2, #list do
-		new[i - 1] = list[i]
+	for i = 1 + shifts, #list do
+		new[i - shifts] = list[i]
 	end
 end
 
