3f36d3d8d7954c33e4d99e8934a9ae53d66f31e0
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 12:09:12 -0500

    Added Dictionary.flip test

---
+++ tests/Llama/Dictionary/flip.spec.lua
@@ -0,0 +1,50 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local Dictionary = Llama.Dictionary
+	local flip = Dictionary.flip
+
+	it("should return a new table", function()
+		local a = {}
+		local b = flip(a)
+
+		expect(a).never.to.equal(b)
+	end)
+
+	it("should not mutate the given table", function()
+		local a = {
+			foo = "foo",
+			bar = "bar",
+			baz = "baz"
+		}
+		local mutations = 0
+
+		setmetatable(a, {
+			__newindex = function()
+				mutations = mutations + 1
+			end
+		})
+
+		flip(a)
+
+		expect(mutations).to.equal(0)
+	end)
+
+	it("should flip keys and values", function()
+		local a = {
+			foo = 1,
+			bar = "yeehaw",
+			baz = "yeet",
+			skeet = "yeet",
+		}
+
+		local b = flip(a)
+
+		for k, v in pairs(b) do
+			expect(k).to.equal(a[v])
+		end
+	end)
+end
