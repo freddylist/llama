cccf1195da701bcabce6937e4295071700a414fa
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 12:07:22 -0500

    Fixed and optimized List.flatMap

--- src/List/flatMap.lua
+++ src/List/flatMap.lua
@@ -1,6 +1,4 @@
 
-local join = require(script.Parent.join)
-
 local function flatMap(list, mapper)
 	local listType = type(list)
 	assert(listType == "table", "expected a table for first argument, got " .. listType)
@@ -8,26 +6,28 @@ local function flatMap(list, mapper)
 	local mapperType = type(mapper)
 	assert(mapperType == "function", "expected a function for second argument, got " .. mapperType)
 
-	local flatMapped = {}
+	local new = {}
 	local index = 1
 
 	for i = 1, #list do
 		if type(list[i]) == "table" then
 			local layer = flatMap(list[i], mapper)
 
-			flatMapped = join(layer, flatMapped)
-			index = index + #layer + 1
+			for j = 1, #layer do
+				new[index] = layer[j]
+				index = index + 1
+			end
 		else
 			local value = mapper(list[i], i)
 
 			if value ~= nil then
-				flatMapped[index] = value
+				new[index] = value
 				index = index + 1
 			end
 		end
 	end
 	
-	return flatMapped
+	return new
 end
 
 return flatMap
