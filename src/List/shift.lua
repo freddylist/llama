d457029c34ffcb7dd7d40816fc20e02ae3285b54
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 14:45:16 -0500

    Fixed List.shift

--- src/List/shift.lua
+++ src/List/shift.lua
@@ -5,13 +5,15 @@ local function shift(list, shifts)
 
 	shifts = shifts or 1
 	local shiftsType = type(shifts)
-	assert(shiftsType == "number" and shiftsType % 1 == 0, "expected an integer for second argument, got " .. shiftsType)
+	assert(shiftsType == "number" and shifts % 1 == 0, "expected an integer for second argument, got " .. shiftsType)
 	
 	local new = {}
 
 	for i = 1 + shifts, #list do
 		new[i - shifts] = list[i]
 	end
+
+	return new
 end
 
 return shift
