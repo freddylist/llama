38880c9aa3fd2c9b0ca50faf2b50657e0cba4654
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 14:46:54 -0500

    Added Set.has

--- src/Set/init.lua
+++ src/Set/init.lua
@@ -3,6 +3,7 @@ local Dictionary = require(script.Parent.Dictionary)
 
 local Set = {
 	add			= require(script.add),
+	has			= require(script.has),
 	intersect	= require(script.intersect),
 	subtract	= require(script.subtract),
 	toList		= require(script.toList),
