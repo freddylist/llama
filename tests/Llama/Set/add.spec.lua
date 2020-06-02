c84acaefdc38c9cb4de9c4121c0e37e99dcd2a05
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 14:51:37 -0500

    Added Set.add test

---
+++ tests/Llama/Set/add.spec.lua
@@ -0,0 +1,44 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local Set = Llama.Set
+	local add = Set.add
+
+	it("should return a new table", function()
+		local a = {}
+
+		expect(add(a, "e")).never.to.equal(a)
+	end)
+	
+	it("should not mutate the given table", function()
+		local a = {
+			foo = true,
+			bar = true,
+			baz = true,
+		}
+		local mutations = 0
+
+		setmetatable(a, {
+			__newindex = function()
+				mutations = mutations + 1
+			end
+		})
+
+		add(a, "foobar")
+
+		expect(mutations).to.equal(0)
+	end)
+
+	it("should add provided entries", function()
+		local a = {}
+
+		local b = add(a, "foo", "bar", "baz")
+
+		expect(b.foo).to.equal(true)
+		expect(b.bar).to.equal(true)
+		expect(b.baz).to.equal(true)
+	end)
+end
