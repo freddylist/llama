4fa8482b04ccdb7ec7cf728cb186392a66e3bef5
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 14:49:51 -0500

    Added List.get test

---
+++ tests/Llama/List/get.spec.lua
@@ -0,0 +1,22 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local List = Llama.List
+	local get = List.get
+
+	it("should get the value at the specified index", function()
+		local a = {
+			4,
+			5,
+			6,
+		}
+
+		expect(get(a, 1)).to.equal(4)
+		expect(get(a, 2)).to.equal(5)
+		expect(get(a, 3)).to.equal(6)
+		expect(get(a, 4)).to.equal(nil)
+	end)
+end
