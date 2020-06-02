86c46692c9105e8562bc278c01d1ae16c3cf71b2
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-02 14:52:09 -0500

    Added Set.subtract test

---
+++ tests/Llama/Set/subtract.spec.lua
@@ -0,0 +1,48 @@
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+	local lib = ReplicatedStorage.lib
+	local Llama = require(lib.Llama)
+
+	local Set = Llama.Set
+	local subtract = Set.subtract
+
+	it("should return a new table", function()
+		local a = {}
+
+		expect(subtract(a, "e")).never.to.equal(a)
+	end)
+	
+	it("should not mutate the given table", function()
+		local a = {
+			foo = true,
+			bar = true,
+			baz = true,
+		}
+		local mutations = 0
+
+		setmetatable(a, {
+			__newindex = function()
+				mutations = mutations + 1
+			end
+		})
+
+		subtract(a, "foobar")
+
+		expect(mutations).to.equal(0)
+	end)
+
+	it("should subtract provided entries", function()
+		local a = {
+			foo = true,
+			bar = true,
+			baz = true,
+		}
+
+		local b = subtract(a, "foo", "bar")
+
+		expect(b.foo).to.equal(nil)
+		expect(b.bar).to.equal(nil)
+		expect(b.baz).to.equal(true)
+	end)
+end
