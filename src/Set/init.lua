c729bd7cd92acf90c7958a6383869804c52bc9ee
F-RDY <51757856+F-RDY@users.noreply.github.com>
2020-06-29 09:52:44 -0500

    Get rid of horizontal equal spacing

--- src/Set/init.lua
+++ src/Set/init.lua
@@ -2,12 +2,12 @@
 local Dictionary = require(script.Parent.Dictionary)
 
 local Set = {
-	add			= require(script.add),
-	has			= require(script.has),
-	intersect	= require(script.intersect),
-	subtract	= require(script.subtract),
-	toList		= require(script.toList),
-	union		= require(script.union),
+	add = require(script.add),
+	has = require(script.has),
+	intersect = require(script.intersect),
+	subtract = require(script.subtract),
+	toList = require(script.toList),
+	union = require(script.union),
 
 	copy = Dictionary.copy,
 }
--- src/init.lua
+++ src/init.lua
@@ -1,13 +1,13 @@
 
 local Llama = {
-	Dictionary		= require(script.Dictionary),
-	List			= require(script.List),
-	Set				= require(script.Set),
+	Dictionary = require(script.Dictionary),
+	List = require(script.List),
+	Set = require(script.Set),
 
-	isEmpty			= require(script.isEmpty),
-	equalObjects	= require(script.equalObjects),
+	isEmpty = require(script.isEmpty),
+	equalObjects = require(script.equalObjects),
 
-	None			= require(script.None),
+	None = require(script.None),
 }
 
 return Llama
