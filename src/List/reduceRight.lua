a2c2bf9ed4a5c69f04b3eefcfa2b74ba687ac2c4
jeparlefrancais <jeparlefrancais21@gmail.com>
2020-12-02 01:14:28 -0500

    allow falsy reduction initial value for reduce and reduceRight

--- src/List/reduceRight.lua
+++ src/List/reduceRight.lua
@@ -2,12 +2,15 @@
 local function reduceRight(list, reducer, initialReduction)
 	local listType = type(list)
 	assert(listType == "table", "expected a table for first argument, got " .. listType)
-	
+
 	local reducerType = type(reducer)
 	assert(reducerType == "function", "expected a function for second argument, got " .. reducerType)
-	
+
 	local len = #list
-	local reduction = initialReduction or list[len]
+	local reduction = initialReduction
+	if reduction == nil then
+		reduction = list[len]
+	end
 	local start = initialReduction == nil and len - 1 or len
 
 	for i = start, 1, -1 do
